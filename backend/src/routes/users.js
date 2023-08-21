a1aeb8dc92553fe5b5e235df26b486ebcec14895
Seong-A <ksa6777@naver.com>
2023-08-21 21:51:18 +0900

    backend basic structure

---
+++ backend/src/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const User = require('../models/User');
+//const Product = require('../models/Product');
+//const Payment = require('../models/Payment');
+const router = express.Router();
+
+
+module.exports = router
