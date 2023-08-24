b8d9ef0c6780d5e500ec1228c418bd2a422300cb
Seong-A <ksa6777@naver.com>
2023-08-24 17:17:47 +0900

    loginpage

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -31,7 +31,14 @@ userSchema.pre('save', async function(next){
         user.password = hash;
     }
     next();
-})
+});
+
+userSchema.methods.comparePassword = async function(plainPassword) {
+    let user = this;
+    console.log(user);
+    const match = await bcrypt.compare(plainPassword,user.password);
+    return match;
+}
 
 const User = mongoose.model("User", userSchema);
 
